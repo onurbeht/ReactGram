b50522eeb0e0535695c7135c40d8b459230c4cdc
Brunn09 <brunnobatista25@gmail.com>
2023-08-29 18:24:19 -0300

    Register and login function created

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -13,9 +13,74 @@ const genereteToken = (id) => {
 
 //Register user and sign in
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  //check if user exists
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({
+      errros: ["Por favor, utilize outro email"],
+    });
+    return;
+  }
+
+  //genereate password hash
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  //create user
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  //if user was created successfully, return the token
+  if (!newUser) {
+    res.status(422).json({
+      errors: ["Houve um erro, por favor tente mais tarde."],
+    });
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: genereteToken(newUser._id),
+  });
+};
+
+//sign user in
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  //check if users exists
+  if (!user) {
+    res.status(404).json({
+      errors: ["Usuários ou senha inválidos"],
+    });
+    return;
+  }
+
+  //check if password matches
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({
+      errors: ["Usuários ou senha inválidos"],
+    });
+    return;
+  }
+
+  //Return user and token
+  res.status(201).json({
+    _id: user._id,
+    profileImage: user.profileImage,
+    token: genereteToken(user._id),
+  });
 };
 
 module.exports = {
   register,
+  login,
 };
---
+++ backend/middlewares/userValidations.js
@@ -0,0 +1,47 @@
+const { body } = require("express-validator");
+
+const userCreateValidation = () => {
+  return [
+    body("name")
+      .isString()
+      .withMessage("O nome é obrigatório!")
+      .isLength({ min: 3 })
+      .withMessage("O nome precisa ter no minimo 3 caracteres!"),
+    body("email")
+      .isString()
+      .withMessage("O email é obrigatório!")
+      .isEmail()
+      .withMessage("Insira um email válido!"),
+    body("password")
+      .isString()
+      .withMessage("A senha é obrigratória!")
+      .isLength({ min: 6 })
+      .withMessage("A senha precisa ter no minimo 6 caracteres!"),
+    body("confirmPass")
+      .isString()
+      .withMessage("A confirmação de senha é obrigatória!")
+      .custom((value, { req }) => {
+        if (value != req.body.password) {
+          throw new Error("As senhas não são iguais");
+        }
+        return true;
+      }),
+  ];
+};
+
+const loginValidation = () => {
+  return [
+    body("email")
+      .isString()
+      .withMessage("O email é obrigatório")
+      .isEmail()
+      .withMessage("Insira um email valido!"),
+    body("password")
+      .isString()
+      .withMessage("A senha é obrigatória")
+      .isLength({ min: 6 })
+      .withMessage("A senha precisa de no minimo 6 caracteres!"),
+  ];
+};
+
+module.exports = { userCreateValidation, loginValidation };
